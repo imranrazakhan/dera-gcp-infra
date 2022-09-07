a368be4550f5f044a6422d259a2dbb4243449e80
imranrazakhan <imranrazakhan@users.noreply.github.com>
2022-09-07 15:10:44 +0200

    initial commit

--- modules/kubernetes/outputs.tf
+++ modules/kubernetes/outputs.tf
@@ -17,3 +17,28 @@ output "cluster_name" {
   description = "Cluster name"
   value       = module.gke.name
 }
+  
+output "kubernetes_endpoint" {
+  sensitive = true
+  value     = module.gke_auth.host
+}
+
+output "client_token" {
+  sensitive = true
+  value     = module.gke_auth.token
+}
+
+output "ca_certificate" {
+  value = module.gke_auth.cluster_ca_certificate
+  sensitive = true 
+}
+
+output "kubeconfig_raw" {
+  value = module.gke_auth.kubeconfig_raw
+  sensitive = true 
+}
+
+output "service_account" {
+  description = "The default service account used for running nodes."
+  value       = module.gke.service_account
+}
