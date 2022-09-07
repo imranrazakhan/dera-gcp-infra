1d3e4551bfe38d170e34b3744251e5b3ab18ccb5
imranrazakhan <imranrazakhan@users.noreply.github.com>
2022-09-07 12:47:49 +0200

    initial commit

--- modules/kubernetes/main.tf
+++ modules/kubernetes/main.tf
@@ -5,3 +5,8 @@ module "gke_auth" {
   location     = module.gke.location
   cluster_name = module.gke.name
 }
+
+resource "local_file" "kubeconfig" {
+  content  = module.gke_auth.kubeconfig_raw
+  filename = "kubeconfig-${var.env_name}"
+}
