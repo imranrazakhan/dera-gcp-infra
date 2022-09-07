807063991c12f276a3f6eadeca03f1fade20f0a0
imranrazakhan <imranrazakhan@users.noreply.github.com>
2022-09-07 12:11:59 +0200

    initial commit

--- modules/kubernetes/variables.tf
+++ modules/kubernetes/variables.tf
@@ -46,6 +46,23 @@ variable "kubernetes_max_ver" {
   default = "latest"
 }
 
+variable "network" {
+  description = "The VPC network created to host the cluster in"
+  default     = "gke-network"
+}
+variable "subnetwork" {
+  description = "The subnetwork created to host the cluster in"
+  default     = "gke-subnet"
+}
+variable "ip_range_pods_name" {
+  description = "The secondary ip range to use for pods"
+  default     = "ip-range-pods"
+}
+variable "ip_range_services_name" {
+  description = "The secondary ip range to use for services"
+  default     = "ip-range-services"
+}
+
 variable "iam_roles_list" {
   description = "List of IAM roles to be assigned to GKE service account"
   type        = list(string)
