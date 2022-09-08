6e76c2ca0180ff68e7dce0b01abf0373b0b3ed16
imranrazakhan <imranrazakhan@users.noreply.github.com>
2022-09-08 19:50:21 +0200

    initial commit

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -62,9 +62,11 @@ variable "kubernetes_max_ver" {
 
 variable "network" {
   description = "The VPC network created to host the cluster in"
+  default     = "dera-gke-network"
 }
 variable "subnetwork" {
   description = "The subnetwork created to host the cluster in"
+  default     = "dera-gke-subnet"
 }
 
 variable "subnetwork_ipv4_cidr_range" {
@@ -74,6 +76,7 @@ variable "subnetwork_ipv4_cidr_range" {
 
 variable "ip_range_pods_name" {
   description = "The secondary ip range to use for pods"
+  default     = "ip-range-pods"
 }
 
 variable "pod_ipv4_cidr_range" {
@@ -84,6 +87,7 @@ variable "pod_ipv4_cidr_range" {
 
 variable "ip_range_services_name" {
   description = "The secondary ip range to use for services"
+  default     = "ip-range-services"
 }
 
 variable "services_ipv4_cidr_range" {
@@ -110,6 +114,7 @@ variable "service_account" {
 variable "initial_node_count" {
   type        = number
   description = "The number of nodes to create in this cluster's default node pool."
+  default = 1
 }
 
 variable "ssh_user" {
