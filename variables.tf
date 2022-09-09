3fee17abd56fcbfd72a9418d4ee5cec2530ffc8f
imranrazakhan <imranrazakhan@users.noreply.github.com>
2022-09-09 17:07:23 +0200

    initial commit

--- variables.tf
+++ variables.tf
@@ -1,16 +1,1 @@
-variable "pods_ip_range_name" {
-  description = "The secondary ip range to use for pods"
-}
 
-variable "pods_ip_cidr_range" {
-  description = "The cidr ip range to use for pods"
-}
-
-
-variable "services_ip_range_name" {
-  description = "The secondary ip range to use for services"
-}
-
-variable "services_ip_cidr_range" {
-  description = "The cidr ip range to use for services"
-}
