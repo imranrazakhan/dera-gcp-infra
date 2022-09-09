284b5c8fe5f56bb42e2ca0dfb71837cb161253e2
imranrazakhan <imranrazakhan@users.noreply.github.com>
2022-09-09 15:13:57 +0200

    initial commit

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -5,7 +5,16 @@ module "vpc" {
 }
   
 module "gke_auth" {
-  source = "../../modules/kubernetes"
+  source = "terraform-google-modules/kubernetes-engine/google//modules/auth"
+  depends_on   = [module.gke]
+  project_id   = var.project_id
+  location     = module.gke.location
+  cluster_name = module.gke.name
+}
+
+resource "local_file" "kubeconfig" {
+  content  = module.gke_auth.kubeconfig_raw
+  filename = "kubeconfig-${var.env_name}"
 }
 
 module "gke" {
