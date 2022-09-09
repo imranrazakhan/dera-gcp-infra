397c3babef29bb50b69a9b084ac4dc0486dbb73c
imranrazakhan <imranrazakhan@users.noreply.github.com>
2022-09-09 14:13:54 +0200

    initial commit

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -4,7 +4,17 @@ module "vpc" {
 
 module "gke" {
   source  = "../../modules/kubernetes"
-  project_id  = var.project_id
+  # required variables
+  project_id                 = var.project_id
+  name                       = var.cluster_name
+  region                     = var.region
+  zones                      = var.zones
+  network                    = module.vpc.network_name
+  subnetwork                 = module.vpc.subnets_names[0]
+  ip_range_pods              = var.ip_range_pods_name              # join("-",[var.subnetwork,"pods"])
+  ip_range_services          = var.ip_range_services_name          # join("-",[var.subnetwork,"services"])
+    
+  create_service_account = false
 }
 
 
