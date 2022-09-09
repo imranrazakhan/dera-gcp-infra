f91e76be79328306b3161eee191d3d23f2d3ba93
imranrazakhan <imranrazakhan@users.noreply.github.com>
2022-09-09 16:55:20 +0200

    initial commit

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -74,26 +74,23 @@ variable "subnetwork_ipv4_cidr_range" {
   default     = "10.20.0.0/14"
 }
 
-variable "ip_range_pods_name" {
+variable "pods_ip_range_name" {
   description = "The secondary ip range to use for pods"
-  default     = "ip-range-pods"
 }
 
-variable "pod_ipv4_cidr_range" {
+variable "pods_ip_cidr_range" {
   description = "The cidr ip range to use for pods"
-  default     = "10.24.0.0/14"
 }
 
 
-variable "ip_range_services_name" {
+variable "services_ip_range_name" {
   description = "The secondary ip range to use for services"
-  default     = "ip-range-services"
 }
 
-variable "services_ipv4_cidr_range" {
+variable "services_ip_cidr_range" {
   description = "The cidr ip range to use for services"
-  default     = "10.28.0.0/20"
 }
+
 
 variable "iam_roles_list" {
   description = "List of IAM roles to be assigned to GKE service account"
