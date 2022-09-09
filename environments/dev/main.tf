1567c287c622f62b4174819e450f49ba5d9caaf0
imranrazakhan <imranrazakhan@users.noreply.github.com>
2022-09-09 14:25:52 +0200

    initial commit

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -4,6 +4,9 @@ module "vpc" {
 
 module "gke" {
   source  = "../../modules/kubernetes"
+  
+  env_name = var.env_name
+  
   # required variables
   project_id                 = var.project_id
   name                       = var.cluster_name
@@ -11,26 +14,25 @@ module "gke" {
   zones                      = var.zones
   network                    = module.vpc.network_name
   subnetwork                 = module.vpc.subnets_names[0]
+    
+  machine_type              = var.machine_type
+  min_count                 = var.min_count
+  max_count                 = var.max_count
+  disk_size_gb    =   var.disk_size_gb
 
-  node_pools = [
-    {
-      name                      = "${var.project_name}-gke-node-pool"
-      machine_type              = var.machine_type
-      node_locations            = "europe-west1-b,europe-west1-c,europe-west1-d"
-      min_count                 = var.min_count
-      max_count                 = var.max_count
-      local_ssd_count           = var.disk_size_gb
-      disk_type                 = "pd-standard"
-      image_type                = "COS_CONTAINERD"
-      auto_repair               = true
-      auto_upgrade              = true
-      # service_account           = var.service_account
-      #   service_account           = "project-service-account@<PROJECT ID>.iam.gserviceaccount.com"
-      preemptible               = false
-      initial_node_count        = var.initial_node_count
-    },
-  ]
-
+  pod_ipv4_cidr_range  = var.pod_ipv4_cidr_range
+  initial_node_count        = var.initial_node_count
+    
+  ip_range_services_name = var.ip_range_services_name
+  subnetwork_ipv4_cidr_range  = var.subnetwork_ipv4_cidr_range
+    
+  ip_range_pods_name  = var.ip_range_pods_name
+    
+  services_ipv4_cidr_range  = var.services_ipv4_cidr_range
+    
+  service_account = var.service_account
+    
+  
 }
 
 
