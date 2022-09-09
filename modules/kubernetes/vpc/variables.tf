98d47214cc8d7bdc8e07ceb8f323acfcd302ed7f
imranrazakhan <imranrazakhan@users.noreply.github.com>
2022-09-09 15:55:30 +0200

    initial commit

--- modules/kubernetes/vpc/variables.tf
+++ modules/kubernetes/vpc/variables.tf
@@ -5,3 +5,28 @@ variable "project_id" {
 variable "network_name" {
   description = "The name of the VPC network being created"
 }
+
+variable "env_name" {
+  description = "The environment for the GKE cluster"
+}
+
+variable "subnetwork" {
+  description = "The subnetwork created to host the cluster in"
+}
+
+variable "ip_range_pods_name" {
+  description = "The secondary ip range to use for pods"
+}
+
+variable "pod_ipv4_cidr_range" {
+  description = "The cidr ip range to use for pods"
+}
+
+
+variable "ip_range_services_name" {
+  description = "The secondary ip range to use for services"
+}
+
+variable "services_ipv4_cidr_range" {
+  description = "The cidr ip range to use for services"
+}
